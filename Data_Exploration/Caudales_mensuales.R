44b75ec00a1d88582082a94f33b7f871472d5c6b
Harold <hllauca@gmail.com>
2019-04-05 11:42:31 -0500

    Add files via upload

---
+++ Data_Exploration/Caudales_mensuales.R
@@ -0,0 +1,33 @@
+# This script is used to convert daily to monthly data
+# By Harold Llauca
+
+# Remove stored variables and clean console
+rm(list=ls())
+cat('\f')
+
+
+# Enter variables
+wd        <- 'D:/GR2M_PERU/Caudales'
+file.load <- 'Q_diario_titicaca.csv'
+file.save <- 'Q_mensual_titicaca.csv'
+threshold <- 5
+
+
+# Read file with daily data
+setwd(wd)
+df.raw <- read.table(file.load, sep=',', header=F) # Leer tabla
+
+
+# Extract metadata
+Metadata <- df.raw[1:5,]
+Qdaily   <- df.raw[-5:-1,]
+
+
+# Convert daily to monthly data
+source('convert_d2m.R')
+Qmonthly <- convert_d2m(Qdaily, threshold, FUN='mean')
+
+
+# Save monthly data as .csv
+ans <- rbind(as.matrix(Metadata), as.matrix(Qmonthly))
+write.table(ans, file.save, sep=',', row.names=F, col.names=F)
